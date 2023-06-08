ff6b18ddfcd2d762701f70795f78ce9135fcd236
janasatya <91140090+janasatya@users.noreply.github.com>
2023-06-08 19:03:36 +0530

    sadgas

---
+++ client/src/pages/components/My-Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Styles from "./homepage1.module.css";
+import Link from "next/link";
+import Web3modal from 'web3modal'
+import { ethers } from "ethers";
+
+
+export default function Navbar({connect}){
+    const [webApi,setWebApi]=useState({provider:null,signer:null,contract:null})
+
+    return (
+        <div>
+        <header>
+          <nav className="flex justify-between items-center">
+            <Link href="/">
+              <button type="button">
+                <img className="w-28 mt-8 h-24 ml-16" src="/logo.png" />
+              </button>
+            </Link>
+  
+            <ul className="flex justify-center align-center ">
+              <li>
+                <button
+                  type="button"
+                  className="shadow-md mr-10 text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-12 mb-2"
+                onClick={connect}>
+                  Connect Wallet
+                </button>
+                
+              </li>
+            </ul>
+          </nav>
+        </header>
+  </div>
+    )
+}
---
+++ client/src/pages/components/Policy-1.js
@@ -0,0 +1,103 @@
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import Form from "./Form";
+
+export default function Policy1({account,webApi}) {
+  return (
+    <>
+      <div className="w-5/6 m-auto">
+        <p className="text-center text-xl font-bold">1st POLICY (4 MONTHS)</p>
+        <div className="flex justify-between">
+          <div className="w-2/5 inline-block">
+            <p className="text-center text-lg font-bold p-1">Policy view</p>
+            <table className="text-lg">
+              <thead>
+                <tr>
+                  <th className="w-1/3 border-2 border-black ">Months</th>
+                  <th className="w-1/3 border-2 border-black text-base">
+                    EMI (monthly) (in USDT) (per month) [10% of area (in
+                    sq.feet)]
+                  </th>
+                  <th className="w-1/3 border-2 border-black ">Duration</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border-2 border-black p-1">1st month</td>
+                  <td className="border-2 border-black p-1">10000</td>
+                  <td className="border-2 border-black p-1">[1day-30day]</td>
+                </tr>
+                <tr>
+                  <td className="border-2 border-black p-1">2nd month</td>
+                  <td className="border-2 border-black p-1">10000</td>
+                  <td className="border-2 border-black p-1">[31day-60day]</td>
+                </tr>
+                <tr>
+                  <td className="border-2 border-black p-1">3rd month</td>
+                  <td className="border-2 border-black p-1">10000</td>
+                  <td className="border-2 border-black p-1">[61day-90day]</td>
+                </tr>
+                <tr>
+                  <td className="border-2 border-black p-1">4th month</td>
+                  <td className="border-2 border-black p-1">10000</td>
+                  <td className="border-2 border-black p-1">[91day-120day]</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="w-2/5 inline-block">
+            <p className="text-center text-lg font-bold p-1">
+              Claim Settlement
+            </p>
+            <table className="text-lg w-full">
+              <thead>
+                <tr>
+                  <th className="w-1/3 border-2 border-black ">
+                    Windspeed (per hour)
+                  </th>
+                  <th className="w-1/3 border-2 border-black ">
+                    Rainfall (in mm)
+                  </th>
+                  <th className="w-1/3 border-2 border-black ">
+                    Status (in usdt)
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border-2 border-black p-1">Less than 45km</td>
+                  <td className="border-2 border-black p-1">
+                    Less than 300-400 mm
+                  </td>
+                  <td className="border-2 border-black p-1">NULL</td>
+                </tr>
+                <tr>
+                  <td className="border-2 border-black p-1">46km-87km </td>
+                  <td className="border-2 border-black p-1">401mm-500mm</td>
+                  <td className="border-2 border-black p-1">80,000</td>
+                </tr>
+                <tr>
+                  <td className="border-2 border-black p-1">88km-102km</td>
+                  <td className="border-2 border-black p-1">501mm-600mm </td>
+                  <td className="border-2 border-black p-1">1,00,000</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <Popup
+          trigger={
+            <button className="text-lg p-4 border-2 border-black w-full block m-auto my-4">
+              Register
+            </button>
+          }
+          modal nested
+        >{
+            close=>(<Form account={account} webApi={webApi} months={4}/>)
+        }
+
+        </Popup>
+      </div>
+    </>
+  );
+}
